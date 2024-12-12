4581c4cbe3da4421a3941fc2baaf004ac59f4a21
mohamed.emad <mohammedemad557@yahoo.com>
2024-12-12 02:02:16 +0200

    hive adaptor - box - put delete clear  box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_in_flutter/boxes.dart';
+import 'package:hive_in_flutter/person.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter(PersonAdapter());
+  peronsBox = await Hive.openBox<Person>('persons');
+
   runApp(const MyApp());
 }
 
@@ -12,43 +17,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: MyHomePage(title: 'Flutter hive'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -57,71 +33,137 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: Colors.blue,
+        title: Text(
+          widget.title,
+          style: const TextStyle(fontSize: 32, color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          color: Colors.blue,
+          child: Column(
+            children: [
+              Container(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        hintText: 'name',
+                        border: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.grey.withOpacity(0.5)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.grey.withOpacity(0.5)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      controller: ageController,
+                      decoration: InputDecoration(
+                        hintText: 'age',
+                        border: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.grey.withOpacity(0.5)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.grey.withOpacity(0.5)),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          ///add person to boxPerson
+                          if (nameController.text.isNotEmpty &&
+                              ageController.text.isNotEmpty) {
+                            setState(() {
+                              peronsBox.put(
+                                'key_${nameController.text}',
+                                Person(
+                                    name: nameController.text,
+                                    age: int.parse(ageController.text)),
+                              );
+                            });
+                          }
+                        },
+                        child: const Text('add')),
+                  ],
+                ),
+              ),
+              Expanded(
+                  child: ListView.builder(
+                itemCount: peronsBox.length,
+                itemBuilder: (context, index) {
+                  Person person = peronsBox.getAt(index);
+                  return Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    color: Colors.white,
+                    child: ListTile(
+                      title: Row(
+                        children: [
+                          Text('name: ${person.name}'),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Text('age: ${person.age}'),
+                        ],
+                      ),
+                      trailing: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              peronsBox.deleteAt(index);
+                            });
+                          },
+                          icon: const Icon(Icons.remove)),
+                    ),
+                  );
+                },
+              )),
+              Container(
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                          style: const ButtonStyle(
+                              backgroundColor:
+                                  WidgetStatePropertyAll(Colors.blue)),
+                          onPressed: () {
+                            setState(() {
+                              peronsBox.clear();
+
+                            });
+                          },
+                          child: const Text(
+                            'delete all',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/person.dart
@@ -0,0 +1,15 @@
+import 'package:hive/hive.dart';
+
+part 'person.g.dart';
+
+@HiveType(typeId: 1)
+class Person {
+
+  @HiveField(0)
+  final String name;
+
+  @HiveField(1)
+  final int age;
+
+  Person({required this.name,required this.age});
+}
